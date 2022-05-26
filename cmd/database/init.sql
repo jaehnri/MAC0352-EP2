18530ce5d068c246687e8e2766fdd3e288348bb6
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-05-26 20:55:48 -0300

    Add state and connection fields to user in SQL

--- cmd/database/init.sql
+++ cmd/database/init.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS players (
     id uuid,
     name varchar(250) NOT NULL,
     password varchar(250) NOT NULL,
+    state ENUM('offline', 'online-availale', 'online-playing') NOT NULL,
+    connected_ip STRING NULL,
+    connected_port STRING NULL,
+    points INT NOT NULL,
     PRIMARY KEY (id)
 );
