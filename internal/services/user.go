15fd3b67cc786e26a676061d7770fa41946e1d72
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-05-26 20:30:42 -0300

    Complete UserService method headers

--- internal/services/user.go
+++ internal/services/user.go
@@ -1,5 +1,11 @@
 package services
 
+type UserData struct {
+	Username string
+	State    string
+	Points   int
+}
+
 type User interface {
 	Create(name string, password string)
 	ChangePassword(name string, password string)
@@ -13,18 +19,31 @@ func NewUserService() *UserService {
 	return &UserService{}
 }
 
-func (u *UserService) Create(name string, password string) {
+func (u *UserService) Create(name string, password string) error {
 	// TODO: Implement user creation
+	return nil
 }
 
-func (u *UserService) ChangePassword(name string, password string) {
+func (u *UserService) ChangePassword(name string, oldpassword, newpassword string) error {
 	// TODO: Implement password change
+	return nil
 }
 
-func (u *UserService) Login(name string) {
+func (u *UserService) Login(name string, password string) error {
 	// TODO: Implement login
+	return nil
 }
 
 func (u *UserService) Logout(name string) {
 	// TODO: Implement logout
+}
+
+func (u *UserService) ListConnected() []UserData {
+	// TODO: Implement logout
+	return nil
+}
+
+func (u *UserService) ListAll() []UserData {
+	// TODO: Implement logout
+	return nil
 }
