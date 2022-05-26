acc9d4c3506a579a2838f71273f2ef51447dfdc3
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-05-26 20:58:13 -0300

    Call all the necessary methods in client main

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	"ep2/internal/game"
+	"ep2/internal/services"
 	"fmt"
-	"io"
+	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -13,21 +16,20 @@ type userState struct {
 	isLogged bool
 	username string
 	// connection
-	isConnected bool
-	connection  io.Reader
-	// game Game
+	inGame bool
+	conn   net.Conn
+	game   game.Game
 }
-
-// TODO remove (this can be added to empty blocks to facilitate development)
-func none() {}
 
 func main() {
 	state := userState{
-		isLogged:    false,
-		username:    "",
-		isConnected: false,
-		connection:  nil,
+		isLogged: false,
+		inGame:   false,
 	}
+
+	userService := services.NewUserService()
+	gameService := services.NewGameService()
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Printf("JogoDaVelha> ")
@@ -40,60 +42,110 @@ func main() {
 
 		switch words[0] {
 		// USER
-		case "new", "l", "halloffame":
-			// TODO
+		case "new":
+			{
+				err := userService.Create(words[1], words[2])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Usuário criado com sucesso")
+				}
+			}
 		case "in":
 			if state.isLogged {
 				fmt.Println("Você já está logado. Faça logout para trocar de usuário.")
 			} else {
-				// TODO
-				state.isLogged = true
+				username := words[1]
+				err := userService.Login(username, words[2])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					state.isLogged = true
+					state.username = username
+					fmt.Printf("Você está logado como '%s'\n", username)
+				}
 			}
 		case "pass":
 			if !state.isLogged {
 				fmt.Println("Você não está logado.")
 			} else {
-				// TODO
-				none()
+				err := userService.ChangePassword(state.username, words[1], words[2])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Sua senha foi alterada.")
+				}
 			}
 		case "out":
-			if state.isConnected {
+			if state.inGame {
 				fmt.Println("Você está em um jogo.")
 			} else if !state.isLogged {
 				fmt.Println("Você não está logado.")
 			} else {
+				userService.Logout(state.username)
 				state.isLogged = false
 				state.username = ""
+			}
+		case "l":
+			fmt.Println("Usuários conectados:")
+			for _, user := range userService.ListConnected() {
+				fmt.Printf("• %s (%s)", user.Username, user.State)
+			}
+		case "halloffame":
+			fmt.Println("Usuários conectados:")
+			for i, user := range userService.ListConnected() {
+				fmt.Printf("%d. %s (%d pts)", i, user.Username, user.Points)
 			}
 
 		// GAME
 		case "call":
-			if !state.isConnected {
+			if !state.inGame {
 				fmt.Println("Faça login antes de iniciar um jogo.")
 			} else {
-				// TODO
-				state.isConnected = true
+				user, err := userService.Get(words[1])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					if user.State != services.Availale {
+						fmt.Printf("O usuário '%s' não está disponível.", user.Username)
+					} else {
+						state.conn = gameService.Connect(user.ConnectedIp, user.ConnectedPort)
+						state.inGame = true
+						state.game = game.NewGame(game.X) // TODO
+					}
+				}
 			}
 		case "play":
-			if !state.isConnected {
+			if !state.inGame {
 				fmt.Println("Você não está em um jogo.")
 			} else {
-				// TODO
-				none()
+				i, erri := strconv.ParseInt(words[1], 10, 32)
+				j, errj := strconv.ParseInt(words[2], 10, 32)
+				if erri != nil || errj != nil {
+					fmt.Println("Posição inválida.")
+				} else {
+					gameService.SendPlay(int(i), int(j))
+					err := state.game.Play(int(i), int(j))
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						fmt.Printf("Você colocou %s em (%d,%d).\n", state.game.User, i, j)
+					}
+				}
 			}
 		case "delay":
-			if !state.isConnected {
+			if !state.inGame {
 				fmt.Println("Você não está em um jogo.")
 			} else {
-				// TODO
-				none()
+				fmt.Printf("A latência é de %d millisegundos.\n", gameService.Delay)
 			}
 		case "over":
-			if !state.isConnected {
+			if !state.inGame {
 				fmt.Println("Você não está em um jogo.")
 			} else {
-				// TODO
-				state.isConnected = false
+				gameService.Disconnect(state.conn)
+				state.inGame = false
+				state.conn = nil
 			}
 
 		// OTHER
