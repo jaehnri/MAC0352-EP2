11fbc13e06c72b086a4d4f45b81152b324d1c8fe
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-05-26 16:45:14 -0300

    Create client shell

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("oi")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Printf("JogoDaVelha> ")
+		scanner.Scan()
+		line := strings.TrimSpace(scanner.Text())
+		words := strings.Split(line, " ")
+		if line == "" || len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "call", "new", "pass", "in", "halloffame", "l", "play", "delay", "over", "out", "bye":
+			fmt.Printf("Esse comando ainda não foi programado. Vindo em breve...\n")
+		default:
+			fmt.Printf("'%s' não é um comando conhecido.\n", words[0])
+		}
+	}
 }
