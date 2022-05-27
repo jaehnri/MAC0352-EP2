b638a1aa85413dcbe0975b549ab778bbc0ee8ef1
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-05-27 03:39:42 -0300

    Refactor client shell: create Client struct to handle all methods

---
+++ cmd/client/core.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"ep2/internal/game"
+	"ep2/internal/services"
+	"errors"
+	"fmt"
+	"net"
+	"strconv"
+)
+
+type stateStruct struct {
+	// login
+	isLogged bool
+	username string
+	// connection
+	inGame bool
+	conn   net.Conn
+	game   game.Game
+}
+
+type Client struct {
+	state       stateStruct
+	userService services.UserService
+	gameService services.GameService
+}
+
+func NewClient() Client {
+	return Client{
+		state: stateStruct{
+			isLogged: false,
+			inGame:   false,
+		},
+		userService: *services.NewUserService(),
+		gameService: *services.NewGameService(),
+	}
+}
+
+// /////////////////////////////////////////////////////////////////////
+// USER
+// /////////////////////////////////////////////////////////////////////
+
+func (c *Client) handleNew(params []string) error {
+	err := c.userService.Create(params[0], params[1])
+	if err != nil {
+		return err
+	}
+	fmt.Println("Usuário criado com sucesso")
+	return nil
+}
+func (c *Client) handleIn(params []string) error {
+	if c.state.isLogged {
+		return errors.New("você já está logado, faça logout para trocar de usuário")
+	}
+	username := params[0]
+	err := c.userService.Login(username, params[1])
+	if err != nil {
+		return err
+	}
+	c.state.isLogged = true
+	c.state.username = username
+	fmt.Printf("Você está logado como '%s'\n", username)
+	return nil
+}
+func (c *Client) handlePass(params []string) error {
+	if !c.state.isLogged {
+		return errors.New("você não está logado")
+	}
+	err := c.userService.ChangePassword(c.state.username, params[0], params[1])
+	if err != nil {
+		return err
+	}
+	fmt.Println("Sua senha foi alterada.")
+	return nil
+}
+func (c *Client) handleOut(params []string) error {
+	if c.state.inGame {
+		return errors.New("você está em um jogo")
+	}
+	if !c.state.isLogged {
+		return errors.New("você não está logado")
+	}
+	c.userService.Logout(c.state.username)
+	c.state.isLogged = false
+	c.state.username = ""
+	return nil
+}
+func (c *Client) handleL(params []string) error {
+	fmt.Println("Usuários conectados:")
+	for _, user := range c.userService.ListConnected() {
+		fmt.Printf("• %s (%s)", user.Username, user.State)
+	}
+	return nil
+}
+func (c *Client) handleHalloffame(params []string) error {
+	fmt.Println("Usuários conectados:")
+	for i, user := range c.userService.ListConnected() {
+		fmt.Printf("%d. %s (%d pts)", i, user.Username, user.Points)
+	}
+	return nil
+}
+
+// /////////////////////////////////////////////////////////////////////
+// GAME
+// /////////////////////////////////////////////////////////////////////
+
+func (c *Client) handleCall(params []string) error {
+	if !c.state.inGame {
+		return errors.New("faça login antes de iniciar um jogo")
+	}
+	user, err := c.userService.Get(params[0])
+	if err != nil {
+		return err
+	}
+	if user.State != services.Availale {
+		return fmt.Errorf("o usuário '%s' não está disponível", user.Username)
+	}
+	c.state.conn = c.gameService.Connect(user.ConnectedIp, user.ConnectedPort)
+	c.state.inGame = true
+	c.state.game = game.NewGame(game.X) // TODO
+	return nil
+
+}
+func (c *Client) handlePlay(params []string) error {
+	if !c.state.inGame {
+		return errors.New("você não está em um jogo")
+	}
+	i, erri := strconv.ParseInt(params[0], 10, 32)
+	j, errj := strconv.ParseInt(params[1], 10, 32)
+	if erri != nil || errj != nil {
+		return errors.New("posição inválida")
+	}
+	c.gameService.SendPlay(int(i), int(j))
+	err := c.state.game.Play(int(i), int(j))
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Você colocou %s em (%d,%d).\n", c.state.game.User, i, j)
+	return nil
+}
+func (c *Client) handleDelay(params []string) error {
+	if !c.state.inGame {
+		return errors.New("você não está em um jogo")
+	}
+	fmt.Printf("A latência é de %d millisegundos.\n", c.gameService.Delay)
+	return nil
+}
+func (c *Client) handleOver(params []string) error {
+	if !c.state.inGame {
+		return errors.New("você não está em um jogo")
+	}
+	c.gameService.Disconnect(c.state.conn)
+	c.state.inGame = false
+	c.state.conn = nil
+	return nil
+}
+
+// /////////////////////////////////////////////////////////////////////
+// CONCURRENCY
+// /////////////////////////////////////////////////////////////////////
+
+func (c *Client) heartbeat() {
+}
+func (c *Client) listenCalled() {
+}
+func (c *Client) listenOver() {
+}
+func (c *Client) listenPlay() {
+}
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,34 +2,13 @@ package main
 
 import (
 	"bufio"
-	"ep2/internal/game"
-	"ep2/internal/services"
 	"fmt"
-	"net"
 	"os"
-	"strconv"
 	"strings"
 )
 
-type userState struct {
-	// login
-	isLogged bool
-	username string
-	// connection
-	inGame bool
-	conn   net.Conn
-	game   game.Game
-}
-
 func main() {
-	state := userState{
-		isLogged: false,
-		inGame:   false,
-	}
-
-	userService := services.NewUserService()
-	gameService := services.NewGameService()
-
+	client := NewClient()
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Printf("JogoDaVelha> ")
@@ -39,121 +18,47 @@ func main() {
 		if line == "" || len(words) == 0 {
 			continue
 		}
+		params := words[1:]
 
 		switch words[0] {
 		// USER
 		case "new":
-			{
-				err := userService.Create(words[1], words[2])
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println("Usuário criado com sucesso")
-				}
-			}
+			handleError(client.handleNew(params))
 		case "in":
-			if state.isLogged {
-				fmt.Println("Você já está logado. Faça logout para trocar de usuário.")
-			} else {
-				username := words[1]
-				err := userService.Login(username, words[2])
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					state.isLogged = true
-					state.username = username
-					fmt.Printf("Você está logado como '%s'\n", username)
-				}
-			}
+			handleError(client.handleIn(params))
 		case "pass":
-			if !state.isLogged {
-				fmt.Println("Você não está logado.")
-			} else {
-				err := userService.ChangePassword(state.username, words[1], words[2])
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					fmt.Println("Sua senha foi alterada.")
-				}
-			}
+			handleError(client.handlePass(params))
 		case "out":
-			if state.inGame {
-				fmt.Println("Você está em um jogo.")
-			} else if !state.isLogged {
-				fmt.Println("Você não está logado.")
-			} else {
-				userService.Logout(state.username)
-				state.isLogged = false
-				state.username = ""
-			}
+			handleError(client.handleOut(params))
 		case "l":
-			fmt.Println("Usuários conectados:")
-			for _, user := range userService.ListConnected() {
-				fmt.Printf("• %s (%s)", user.Username, user.State)
-			}
+			handleError(client.handleL(params))
 		case "halloffame":
-			fmt.Println("Usuários conectados:")
-			for i, user := range userService.ListConnected() {
-				fmt.Printf("%d. %s (%d pts)", i, user.Username, user.Points)
-			}
+			handleError(client.handleHalloffame(params))
 
 		// GAME
 		case "call":
-			if !state.inGame {
-				fmt.Println("Faça login antes de iniciar um jogo.")
-			} else {
-				user, err := userService.Get(words[1])
-				if err != nil {
-					fmt.Println(err)
-				} else {
-					if user.State != services.Availale {
-						fmt.Printf("O usuário '%s' não está disponível.", user.Username)
-					} else {
-						state.conn = gameService.Connect(user.ConnectedIp, user.ConnectedPort)
-						state.inGame = true
-						state.game = game.NewGame(game.X) // TODO
-					}
-				}
-			}
+			handleError(client.handleCall(params))
 		case "play":
-			if !state.inGame {
-				fmt.Println("Você não está em um jogo.")
-			} else {
-				i, erri := strconv.ParseInt(words[1], 10, 32)
-				j, errj := strconv.ParseInt(words[2], 10, 32)
-				if erri != nil || errj != nil {
-					fmt.Println("Posição inválida.")
-				} else {
-					gameService.SendPlay(int(i), int(j))
-					err := state.game.Play(int(i), int(j))
-					if err != nil {
-						fmt.Println(err)
-					} else {
-						fmt.Printf("Você colocou %s em (%d,%d).\n", state.game.User, i, j)
-					}
-				}
-			}
+			handleError(client.handlePlay(params))
 		case "delay":
-			if !state.inGame {
-				fmt.Println("Você não está em um jogo.")
-			} else {
-				fmt.Printf("A latência é de %d millisegundos.\n", gameService.Delay)
-			}
+			handleError(client.handleDelay(params))
 		case "over":
-			if !state.inGame {
-				fmt.Println("Você não está em um jogo.")
-			} else {
-				gameService.Disconnect(state.conn)
-				state.inGame = false
-				state.conn = nil
-			}
+			handleError(client.handleOver(params))
 
 		// OTHER
 		case "bye":
 			fmt.Printf("Fechando o programa...")
 			os.Exit(0)
+		case "help":
+			PrintCommandExplanation()
 		default:
 			fmt.Printf("'%s' não é um comando conhecido.\n", words[0])
 		}
+	}
+}
+
+func handleError(err error) {
+	if err != nil {
+		fmt.Println(err)
 	}
 }
