97d32b9643bd1348696b7539cb7dc4b738bd0084
Luca Dillenburg <luca.assumpcao.dillenburg@gmail.com>
2022-06-04 06:00:39 -0300

    Create interface for ServerConnection

--- internal/client/conn/server.go
+++ internal/client/conn/server.go
@@ -11,6 +11,23 @@ import (
 	"strconv"
 )
 
+type IServerConnection interface {
+	SendStartedGame(username string, oponentUsername string) error
+	SendWon(username string, oponent string) error
+	SendDraw(username string, oponent string) error
+	SendOver(username string, oponent string) error
+	CreateUser(username string, password string) error
+	ChangePassword(username string, oldpassword, newpassword string) error
+	Login(username string, password string) error
+	Logout(username string) error
+	OnlineUsers() ([]model.UserData, error)
+	AllUsers() ([]model.UserData, error)
+	GetUser(username string) (model.UserData, error)
+	ReadHeartbeat() (string, error)
+	SendHeartbeat(username string) error
+	Disconnect() error
+}
+
 type ServerConnection struct {
 	conn            net.Conn
 	writer          bufio.Writer
@@ -63,24 +80,24 @@ func newServerConnection(conn net.Conn, heartbeatConn net.Conn) *ServerConnectio
 // Methods
 /////////////////////////////////////////////////////////////////////////////////////
 
-func (c ServerConnection) SendStartedGame(username string, oponentUsername string) error {
+func (c *ServerConnection) SendStartedGame(username string, oponentUsername string) error {
 	response, err := c.request(fmt.Sprintf("play %s %s", username, oponentUsername))
 	return handleVoidResponse(response, err)
 }
 
-func (c ServerConnection) SendWon(username string, oponent string) error {
+func (c *ServerConnection) SendWon(username string, oponent string) error {
 	return c.sendOver(username, 3, oponent, 0)
 }
 
-func (c ServerConnection) SendDraw(username string, oponent string) error {
+func (c *ServerConnection) SendDraw(username string, oponent string) error {
 	return c.sendOver(username, 1, oponent, 1)
 }
 
-func (c ServerConnection) SendOver(username string, oponent string) error {
+func (c *ServerConnection) SendOver(username string, oponent string) error {
 	return c.sendOver(username, 0, oponent, 0)
 }
 
-func (c ServerConnection) sendOver(username string, usernamePoints int, oponent string, oponentPoints int) error {
+func (c *ServerConnection) sendOver(username string, usernamePoints int, oponent string, oponentPoints int) error {
 	response, err := c.request(fmt.Sprintf("over %s %d %s %d", username, usernamePoints, oponent, oponentPoints))
 	return handleVoidResponse(response, err)
 }
@@ -89,27 +106,27 @@ func (c ServerConnection) sendOver(username string, usernamePoints int, oponent 
 // User
 /////////////////////////////////////////////////////////////////////////////////////
 
-func (c ServerConnection) CreateUser(username string, password string) error {
+func (c *ServerConnection) CreateUser(username string, password string) error {
 	response, err := c.request(fmt.Sprintf("new %s %s", username, password))
 	return handleVoidResponse(response, err)
 }
 
-func (c ServerConnection) ChangePassword(username string, oldpassword, newpassword string) error {
+func (c *ServerConnection) ChangePassword(username string, oldpassword, newpassword string) error {
 	response, err := c.request(fmt.Sprintf("pass %s %s %s", username, oldpassword, newpassword))
 	return handleVoidResponse(response, err)
 }
 
-func (c ServerConnection) Login(username string, password string) error {
+func (c *ServerConnection) Login(username string, password string) error {
 	response, err := c.request(fmt.Sprintf("in %s %s", username, password))
 	return handleVoidResponse(response, err)
 }
 
-func (c ServerConnection) Logout(username string) error {
+func (c *ServerConnection) Logout(username string) error {
 	response, err := c.request(fmt.Sprintf("out %s", username))
 	return handleVoidResponse(response, err)
 }
 
-func (c ServerConnection) OnlineUsers() ([]model.UserData, error) {
+func (c *ServerConnection) OnlineUsers() ([]model.UserData, error) {
 	response, err := c.request("l")
 	if err != nil {
 		return nil, err
@@ -122,7 +139,7 @@ func (c ServerConnection) OnlineUsers() ([]model.UserData, error) {
 	return users, nil
 }
 
-func (c ServerConnection) AllUsers() ([]model.UserData, error) {
+func (c *ServerConnection) AllUsers() ([]model.UserData, error) {
 	response, err := c.request("halloffame")
 	if err != nil {
 		return nil, err
@@ -135,7 +152,7 @@ func (c ServerConnection) AllUsers() ([]model.UserData, error) {
 	return users, nil
 }
 
-func (c ServerConnection) GetUser(username string) (model.UserData, error) {
+func (c *ServerConnection) GetUser(username string) (model.UserData, error) {
 	response, err := c.request(fmt.Sprintf("get %s", username))
 	if err != nil {
 		return model.UserData{}, err
@@ -152,12 +169,12 @@ func (c ServerConnection) GetUser(username string) (model.UserData, error) {
 // Heartbeat
 /////////////////////////////////////////////////////////////////////////////////////
 
-func (c ServerConnection) ReadHeartbeat() (string, error) {
+func (c *ServerConnection) ReadHeartbeat() (string, error) {
 	str, err := c.heartbeatReader.ReadString(config.MessageDelim)
 	return str, err
 }
 
-func (c ServerConnection) SendHeartbeat(username string) error {
+func (c *ServerConnection) SendHeartbeat(username string) error {
 	text := "heartbeat"
 	if username != "" {
 		text += " " + username
@@ -170,7 +187,7 @@ func (c ServerConnection) SendHeartbeat(username string) error {
 // Core
 /////////////////////////////////////////////////////////////////////////////////////
 
-func (c ServerConnection) request(str string) (string, error) {
+func (c *ServerConnection) request(str string) (string, error) {
 	err := c.send(str)
 	if err != nil {
 		return "", err
@@ -180,7 +197,7 @@ func (c ServerConnection) request(str string) (string, error) {
 	return respose, err
 }
 
-func (c ServerConnection) send(str string) error {
+func (c *ServerConnection) send(str string) error {
 	_, err := c.writer.WriteString(config.ParseWriteMessage(str))
 	return err
 }
@@ -195,12 +212,12 @@ func handleVoidResponse(response string, err error) error {
 	return nil
 }
 
-func (c ServerConnection) read() (string, error) {
+func (c *ServerConnection) read() (string, error) {
 	str, err := c.reader.ReadString(config.MessageDelim)
 	return config.ParseMessageRead(str), err
 }
 
-func (c ServerConnection) Disconnect() error {
+func (c *ServerConnection) Disconnect() error {
 	c.send("bye")
 	return c.conn.Close()
 }
